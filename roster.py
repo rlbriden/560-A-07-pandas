f9b8df07d8e7837196b9a63b3e9838b0c1f433e1
Rebecca Bridenbaugh <rlbriden@email.unc.edu>
2024-03-29 14:39:39 -0400

    zero header

--- roster.py
+++ roster.py
@@ -1,8 +1,13 @@
 # https://goheels.com/sports/mens-basketball/roster
 
+import pandas as pd
+
 roster = ['Davis', 'Bacot', 'Ingram']
 
 print(roster)
 
 for player in roster:
     print(player)
+
+data = pd.DataFrame(roster)
+print(data)
