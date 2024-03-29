57212d06df805974c8ca9d9a66d9e867aa05c5d2
Rebecca Bridenbaugh <rlbriden@email.unc.edu>
2024-03-29 14:52:25 -0400

    Last Name header

--- roster.py
+++ roster.py
@@ -2,12 +2,23 @@
 
 import pandas as pd
 
-roster = ['Davis', 'Bacot', 'Ingram']
 
+roster = ['Davis', 'Bacot', 'Ingram']
+player = {'Last Name': roster}
+
+# Creates a dataframe of the players dictionary using pandas, assigns it to a variable named data
+data = pd.DataFrame(player)
+print(data)
+
+# Prints a divider for the output
+print('----------------')
+
+# Prints roster list
 print(roster)
 
+# Prints a divider for the output
+print('----------------')
+
+# Loops through each player in the roster list and prints their name
 for player in roster:
     print(player)
-
-data = pd.DataFrame(roster)
-print(data)
