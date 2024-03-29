e5eeb8c68d2a74df7d0d8ba0722c318f42289ccd
Rebecca Bridenbaugh <rlbriden@email.unc.edu>
2024-03-29 14:59:02 -0400

    removes roster variable

--- roster.py
+++ roster.py
@@ -2,23 +2,11 @@
 
 import pandas as pd
 
-
-roster = ['Davis', 'Bacot', 'Ingram']
-player = {'Last Name': roster}
+player = {'Last Name': ['Davis', 'Bacot', 'Ingram'],
+          'First Name': ['RJ', 'Armando', 'Harrison'],
+          'height': [72, 83, 79],
+          'weight': [180, 240, 225]}
 
 # Creates a dataframe of the players dictionary using pandas, assigns it to a variable named data
 data = pd.DataFrame(player)
 print(data)
-
-# Prints a divider for the output
-print('----------------')
-
-# Prints roster list
-print(roster)
-
-# Prints a divider for the output
-print('----------------')
-
-# Loops through each player in the roster list and prints their name
-for player in roster:
-    print(player)
